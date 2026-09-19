ea85910b981410095dd9879342f35de46651c968
SanZhuss <251355@astanait.edu.kz>
2026-09-20 01:54:51 +0500

    New assignment 2

--- Assignment2/src/factoryPattern/SMSCreator.java
+++ Assignment2/src/factoryPattern/SMSCreator.java
@@ -3,7 +3,7 @@ package factoryPattern;
 public class SMSCreator extends NotificationCreator{
     @Override
     public Notification createNotification(String message, String recipient, String phoneNumber) {
-        return new Push(
+        return new SMS(
                 message,
                 recipient,
                 phoneNumber
